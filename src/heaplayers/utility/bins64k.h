5ff28b5386b7e43174d623497f12c495f8a5ad3d
Emery Berger <emery.berger@gmail.com>
2012-06-11 23:53:25 -0400

    Replaced non-portable bit-scan-reverse computation.

--- src/heaplayers/utility/bins64k.h
+++ src/heaplayers/utility/bins64k.h
@@ -62,18 +62,37 @@ namespace HL {
       _BitScanReverse (&index, i);
       return index;
 #else
-      // FIX ME processor dependent
-      union {
-	double d;
-	struct {
-	  unsigned int mantissal : 32;
-	  unsigned int mantissah : 20;
-	  unsigned int exponent : 11;
-	  unsigned int sign : 1;
+
+#if 1
+      // From http://graphics.stanford.edu/~seander/bithacks.html#ZerosOnRightMultLookup
+      // Works for up to 32 bit sizes.
+      int r;
+      size_t v = i;
+
+      static const int MultiplyDeBruijnBitPosition[32] = 
+	{
+	  0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
+	  8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
 	};
-      } ud;
-      ud.d = (double)(i & ~(i >> 32));  // avoid rounding error
-      return ud.exponent - 1023;
+
+      v |= v >> 1; // first round down to one less than a power of 2 
+      v |= v >> 2;
+      v |= v >> 4;
+      v |= v >> 8;
+      v |= v >> 16;
+
+      r = MultiplyDeBruijnBitPosition[(uint32_t)(v * 0x07C4ACDDU) >> 27];
+
+      return r;
+
+#else // Naive approach.
+      unsigned int r = 0;
+      while (i >>= 1) {
+	r++;
+      }
+      return r;
+#endif
+
 #endif
     }
 
