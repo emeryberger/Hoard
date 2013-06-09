da00897064e41d1c9677a8db0e1934a1f2897270
Emery Berger <emery.berger@gmail.com>
2013-06-09 10:57:57 -0400

    Added more testing.

--- src/include/hoard/geometricsizeclass.cpp
+++ src/include/hoard/geometricsizeclass.cpp
@@ -4,6 +4,10 @@
 int
 main()
 {
-  Hoard::GeometricSizeClass<20> gs;
+  Hoard::GeometricSizeClass<20> gs1;
+  Hoard::GeometricSizeClass<40> gs2;
+  Hoard::GeometricSizeClass<60> gs3;
+  Hoard::GeometricSizeClass<80> gs4;
+  Hoard::GeometricSizeClass<100> gs5;
   return 0;
 }
