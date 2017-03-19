883e620b4679eb19a295528f60e7e4e07c0b9a12
emeryberger <emery.berger@gmail.com>
2017-03-18 20:22:22 -0400

    Removed self-assign.

--- src/source/unixtls.cpp
+++ src/source/unixtls.cpp
@@ -361,7 +361,6 @@ extern "C" int pthread_create (pthread_t *thread,
 {
   // Force initialization of the TLAB before our first thread is created.
   static volatile TheCustomHeapType * t = initializeCustomHeap();
-  t = t;
 
 #if defined(__linux__) || defined(__APPLE__)
   char fname[] = "pthread_create";
