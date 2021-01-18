a1296a531d85dd69e2cd099d256e38d16fa5b9f2
emery.berger@gmail.com <emery.berger@gmail.com>
2021-01-18 13:03:00 -0500

    Larger TLAB.

--- src/include/hoard/hoardconstants.h
+++ src/include/hoard/hoardconstants.h
@@ -19,7 +19,7 @@
 namespace Hoard {
   
   /// The maximum amount of memory that each TLAB may hold, in bytes.
-  enum { MAX_MEMORY_PER_TLAB = 2 * 1024 * 1024UL }; // 2MB
+  enum { MAX_MEMORY_PER_TLAB = 16 * 1024 * 1024UL }; // 16MB
   
   /// The maximum number of threads supported (sort of).
   enum { MaxThreads = 2048 };
@@ -29,7 +29,7 @@ namespace Hoard {
   
   /// Size, in bytes, of the largest object we will cache on a
   /// thread-local allocation buffer.
-  enum { LargestSmallObject = 256UL };
+  enum { LargestSmallObject = 1024UL };
     
 }
 
